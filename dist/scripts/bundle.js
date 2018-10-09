f4acef4919a9b509837d283d0dfdf5b6911d4c39
mirdsm <by_ms19@yahoo.com>
2018-10-09 16:50:07 +0700

    controller view

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -46340,19 +46340,8 @@ module.exports = About;
 "use strict";
 
 var React = require('react');
-var AuthorApi = require('../../api/authorApi');
 
-var Authors = React.createClass({displayName: "Authors",
-	getInitialState: function() {
-		return {
-			authors: []
-		};
-
-	},
-	componentWillMount: function() {
-		this.setState({ authors: AuthorApi.getAllAuthors() });
-	},
-	
+var AuthorsList = React.createClass({displayName: "AuthorsList",
 	render: function() {
 		var createAuthorRow = function(author) {
 			return (
@@ -46374,16 +46363,53 @@ var Authors = React.createClass({displayName: "Authors",
 						React.createElement("th", null, "Status")
 					), 
 					React.createElement("tbody", null, 
-						this.state.authors.map(createAuthorRow, this)
+						this.props.authors.map(createAuthorRow, this)
 					)
 				)
 			)
 		);
 	}
 });
+module.exports = AuthorsList;
+
+},{"react":158}],163:[function(require,module,exports){
+"use strict";
+
+var React = require('react');
+var AuthorApi = require('../../api/authorApi');
+var AuthorList = require('./authorList');
+
+var Authors = React.createClass({displayName: "Authors",
+	getInitialState: function() {
+		return {
+			authors: []
+		};
+
+	},
+	componentDidMount: function() {
+
+
+		if (this.isMounted()) {
+			this.setState({ authors: AuthorApi.getAllAuthors() });
+
+		}
+	},
+	
+	render: function() {
+
+		return (
+			React.createElement("div", null, 
+
+				
+				React.createElement(AuthorList, {authors: this.state.authors})
+			
+			)
+		);
+	}
+});
 module.exports = Authors;
 
-},{"../../api/authorApi":159,"react":158}],163:[function(require,module,exports){
+},{"../../api/authorApi":159,"./authorList":162,"react":158}],164:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -46409,7 +46435,7 @@ var Header = React.createClass({displayName: "Header",
 
 module.exports = Header;
 
-},{"react":158}],164:[function(require,module,exports){
+},{"react":158}],165:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -46427,7 +46453,7 @@ var Home = React.createClass({displayName: "Home",
 
 module.exports = Home;
 
-},{"react":158}],165:[function(require,module,exports){
+},{"react":158}],166:[function(require,module,exports){
 //'use strict';
 $ = jQuery = require('jquery');
 var React = require('react');
@@ -46476,4 +46502,4 @@ module.exports = App;
 React.render(<Home />, document.getElementById('app'));
 */
 
-},{"./components/about/aboutPage":161,"./components/authors/authorPage":162,"./components/common/header":163,"./components/homePage":164,"jquery":1,"react":158}]},{},[165]);
+},{"./components/about/aboutPage":161,"./components/authors/authorPage":163,"./components/common/header":164,"./components/homePage":165,"jquery":1,"react":158}]},{},[166]);
---
+++ src/api/authorSpecific.js
@@ -0,0 +1,18 @@
+module.exports = {
+	coAuthor:
+
+	[
+	{
+		name: 'Subagja',
+		score: '540',
+		date: '2 September 2018'
+
+
+	},
+	{
+		name: 'Giriprasta',
+		score: '3223',
+		date: '12 Oktober 2018'
+	}
+	]
+};
